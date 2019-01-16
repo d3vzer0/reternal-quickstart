8b73ed4fe186e9e30ff6f70b4efeb256172138ab
Joey Dreijer <joey.dreijer@ing.nl>
2019-01-16 21:12:36 +0100

    Added several default commands

--- imports/import_command.py
+++ imports/import_command.py
@@ -3,4 +3,8 @@ from operations import Command
 class ImportCommand:
     def create():
         create_command = Command.create("exec_shell")
+        create_command = Command.create("make_screenshot")
+        create_command = Command.create("get_ifaces")
+        create_command = Command.create("download_remote")
+        create_command = Command.create("read_file")
         return create_command
