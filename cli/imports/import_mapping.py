c1c4bc3acc0b715bff3eb2f481f8b25ff76722de
Joey Dreijer <joey.dreijer@ing.nl>
2019-04-12 14:39:17 +0200

    Denormalised Actors to mapped commands/techniques

--- cli/imports/import_mapping.py
+++ cli/imports/import_mapping.py
@@ -9,7 +9,7 @@ import yaml
 class Mapping:
     def __init__(self, file_path=config['mapping']['path'], extension='.yml'):
         self.file_path = file_path
-        self.extension = '.yml'
+        self.extension = extension
 
     def update(self):
         metta_files = FindFiles.extension(self.file_path, self.extension)
@@ -18,16 +18,21 @@ class Mapping:
                 yaml_object = yaml.load(yamlfile)
                 try:
                     name = yaml_object['name']
-                    platform = yaml_object['mitre_technique']['platform']
-                    reference = yaml_object.get('reference', None)
-                    description = yaml_object.get('description', None)
-                    author = yaml_object.get('author', None)
-                    technique = yaml_object['mitre_technique']['id']
-                    commands = yaml_object['commands']
-                    mitre_object = Mitre('external_id').technique(technique)['data']
-                    all_commands = [{"type":"Mitre", "name":command["type"], "input":command["input"], "sleep":command["sleep"]} for command in commands]
-                    for phase in mitre_object['kill_chain_phases']:
-                        map_result = MapCommand(name).create(mitre_object['technique_id'], mitre_object['name'], technique, phase, platform, all_commands, reference, author, description)
+                    mapping_data = {
+                        'platform': yaml_object['mitre_technique']['platform'],
+                        'reference': yaml_object.get('reference', None),
+                        'description': yaml_object.get('description', None),
+                        'author': yaml_object.get('author', None),
+                        'technique': yaml_object['mitre_technique']['id'],
+                        'commands': yaml_object['commands'],
+                        'external_id': yaml_object['mitre_technique']['id']}
+
+                    technique_object = Mitre('external_id').technique(mapping_data['external_id'])['data']
+                    all_commands = [{"type":"Mitre", "name":command["type"], "input":command["input"],
+                        "sleep":command["sleep"]} for command in mapping_data['commands']]
+
+                    for phase in technique_object['kill_chain_phases']:
+                        map_result = MapCommand(name).create(technique_object, mapping_data, all_commands, phase)
 
                 except Exception as err:
                     pass
