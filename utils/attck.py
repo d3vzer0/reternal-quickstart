9f3a5e897282da6c678f66b857889bde0f505cfb
Joey Dreijer <jdreijer@schubergphilis.com>
2020-10-10 23:53:53 +0200

    Reformatted ATTCK importer script

--- utils/attck.py
+++ utils/attck.py
@@ -1,9 +1,10 @@
 from os import access
-from .environment import config
+from environment import config
 import json
 import aiohttp
 import json
 import asyncio
+
 
 def load_magma(path=config['MAGMA_PATH']):
     magma_mapping = { }
@@ -13,91 +14,81 @@ def load_magma(path=config['MAGMA_PATH']):
             magma_mapping[mapping['external_id']] = mapping
     return magma_mapping
 
-class MitreAttck:
-    def __init__(self, cti_url=config['ATTCK_URL'], api_url=config['API_URL'], magma_path=None, access_token=None):
+
+class Technique:
+    def __init__(self, technique):
+        self.technique = technique
+
+    @classmethod
+    def from_cti(cls, technique):
+        ''' Format technique to match expected API schema '''
+        technique = { 
+            'technique_id': technique['id'],
+            'name':technique['name'],
+            'description': technique['description'],
+            'platforms': technique['x_mitre_platforms'], 
+            'permissions_required': technique.get('x_mitre_permissions_required', []),
+            'data_sources': technique.get('x_mitre_data_sources', []),
+            'references': technique['external_references'],
+            'kill_chain_phases': [phase['phase_name'] for phase in technique['kill_chain_phases']],
+            'is_subtechnique': technique.get('x_mitre_is_subtechnique', False),
+            'data_sources_available': [],
+            'actors': []
+        }
+        return cls(technique)
+
+    def set_magma(self, magma_mapping):
+        external_id = self.technique['external_references'][0]['external_id']
+        if external_id in self.magma_mapping:
+            mapped_usecase = self.magma_mapping[external_id]
+            mapped_usecase.pop('external_id')
+            self.technique['magma'] = mapped_usecase
+
+
+class Actor:
+    def __init__(self, actor):
+        self.actor = actor
+
+    @classmethod
+    def from_cti(cls, actor):
+        actor = {
+            'actor_id': actor['id'], 
+            'name': actor['name'],
+            'references': actor['external_references'],
+            'aliases': actor.get('aliases', None),
+            'description': actor.get('description', None),
+            'techniques': []
+        }
+        return cls(actor)
+
+
+class ImportAttck:
+    def __init__(self, api_url=None, api_token=None):
         ''' Import the MITRE ATTCK techniques and actors from Github '''
-        self.session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False)) #todo fix trusting custom ca
-        self.cti_url = cti_url
+        self.session = None
         self.api_url = api_url
-        self.access_token = access_token
-        self.magma_path = magma_path
-        self.magma_mapping = { }
-        self.actors = { }
-        self.techniques = { }
+        self.api_token = api_token
 
     async def import_technique(self, technique):
         ''' Create technique via Reternal API '''
-        headers = {'Authorization': f'Bearer {self.access_token}'}
+        headers = {'Authorization': f'Bearer {self.api_token}'}
         async with self.session.post(f'{self.api_url}/mitre/techniques', json=technique, headers=headers) as resp:
             if not resp.status == 200:
                 error_message = await resp.json()
                 print(error_message)
 
 
-    def format_technique(self, technique_details):
-        ''' Format technique to match expected API schema '''
-        print(technique_details)
-        external_id = technique_details['external_references'][0]['external_id']
-        killchain = [phase['phase_name'] for phase in technique_details['kill_chain_phases']]
-        technique_data = { 'technique_id': technique_details['id'], 'name':technique_details['name'],
-            'description': technique_details['description'], 'platforms': technique_details['x_mitre_platforms'], 
-            'permissions_required': technique_details.get('x_mitre_permissions_required', []),
-            'data_sources': technique_details.get('x_mitre_data_sources', []),
-            'references': technique_details['external_references'], 'kill_chain_phases': killchain,
-            'data_sources_available': [], 'actors': [], 'is_subtechnique': technique_details.get('x_mitre_is_subtechnique', False)
-        }
-        if external_id in self.magma_mapping:
-            mapped_usecase = self.magma_mapping[external_id]
-            mapped_usecase.pop('external_id')
-            technique_data['magma'] = mapped_usecase
-
-        return technique_data
-
     async def import_actor(self, actor):
         ''' Create actor via Reternal API '''
-        headers = {'Authorization': f'Bearer {self.access_token}'}
+        headers = {'Authorization': f'Bearer {self.api_token}'}
         async with self.session.post(f'{self.api_url}/mitre/actors', json=actor, headers=headers) as resp:
             if not resp.status == 200:
-                print(resp.status)
-
-    def format_actor(self, technique_details):
-        ''' Format technique to match expected API schema '''
-        actor_data = { 'actor_id': technique_details['id'],  'name': technique_details['name'],
-            'references': technique_details['external_references'], 'aliases': technique_details.get('aliases', None),
-            'description': technique_details.get('description', None), 'techniques': []
-        }
-        return actor_data
-
-    def denormalize(self, relations):
-        ''' Denormalize relationship between actors and techniques '''
-        for relation in relations:
-            if 'intrusion-set' in relation['source_ref'] and 'attack-pattern' in relation['target_ref']:
-                technique_object = self.techniques.get(relation['target_ref'], None)
-                actor_object = self.actors[relation['source_ref']]
-                if technique_object:
-                    technique_object['actors'].append({'actor_id': actor_object['actor_id'], 'name': actor_object['name']})
-                    actor_object['techniques'].append({'technique_id': technique_object['technique_id'], 'name': technique_object['name']})
+                error_message = await resp.json()
+                print(error_message)
     
     async def __aenter__(self):
         ''' Initialize session and populate techniques and actors '''
-        if self.magma_path:
-            self.magma_mapping = load_magma(self.magma_path)
-
-        async with self.session.get(self.cti_url) as resp:
-            # Directly loading resp as json will raise an aiohttp exception
-            # Github returns json but with incorrect mimetype (text/html). Read response as text first
-            all_techniques = await resp.text()
-    
-        relations = []
-        for technique_details in json.loads(all_techniques)['objects']:
-            if technique_details.get('revoked', False) == False:
-                if technique_details['type'] == 'attack-pattern': 
-                    self.techniques[technique_details['id']] = self.format_technique(technique_details)
-                elif technique_details['type'] == 'intrusion-set':
-                    self.actors[technique_details['id']] =  self.format_actor(technique_details)
-                elif technique_details['type'] == 'relationship':
-                    relations.append(technique_details)
-        self.denormalize(relations)
+        self.session = aiohttp.ClientSession(connector=aiohttp.TCPConnector(ssl=False))
         return self
 
     async def __aexit__(self, exc_type, exc_value, exc_traceback):
@@ -105,13 +96,59 @@ class MitreAttck:
         await self.session.close()
 
 
+class MitreAttck:
+    def __init__(self, actors = None, techniques = None):
+        self.actors = actors
+        self.techniques = techniques
+
+    @staticmethod
+    def __make_related(actors, techniques, relationships):
+        ''' Denormalize relationship between actors and techniques '''
+        for relationship in relationships:
+            related_technique = techniques.get(relationship['target_ref'], None)
+            related_actor = actors.get(relationship['source_ref'], None)
+            if related_actor and related_technique:
+                related_technique.technique['actors'].append({
+                    'actor_id': related_actor.actor['actor_id'],
+                    'name': related_actor.actor['name']
+                })
+                related_actor.actor['techniques'].append({
+                    'technique_id': related_technique.technique['technique_id'],
+                    'name': related_technique.technique['name']
+                })
+        return actors, techniques
+
+    @classmethod
+    async def from_cti(cls, cti_url='https://raw.githubusercontent.com/mitre/cti/master/enterprise-attack/enterprise-attack.json'):
+        # Since github does not return a valid json response header
+        # we have to load the response as text first and parse afterwards
+        async with aiohttp.ClientSession() as session:
+            async with session.get(cti_url) as resp:
+                get_entries = await resp.text()
+
+        actors, techniques, relationships = {}, {}, []
+        for entry in json.loads(get_entries)['objects']:
+            if entry.get('revoked', False) == False:
+                if entry['type'] == 'attack-pattern':
+                    techniques[entry['id']] = Technique.from_cti(entry)
+                elif entry['type'] == 'intrusion-set':
+                    actors[entry['id']] = Actor.from_cti(entry)
+                elif entry['type'] == 'relationship':
+                    relationships.append(entry)
+
+        actors, techniques = cls.__make_related(actors, techniques, relationships)
+        return cls(actors, techniques)
+    
+
 async def import_attck(*args, **kwargs):
     ''' Retrieve MITRE ATTCK database and format data '''
-    async with MitreAttck(**kwargs) as mitre:
-        for technique in mitre.techniques.values():
-            await mitre.import_technique(technique)
-        for actor in mitre.actors.values():
-            await mitre.import_actor(actor)
+    mitre_attck = await MitreAttck.from_cti(config['CTI_URL'])
+    async with ImportAttck(api_url=config['API_URL'], api_token=config['API_TOKEN']) as importer:
+        for technique in mitre_attck.techniques.values():
+            await importer.import_technique(technique.technique)
+
+        for actor in mitre_attck.actors.values():
+            await importer.import_actor(actor.actor)
 
 if __name__ == "__main__":
-    asyncio.run(import_attack())
+    asyncio.run(import_attck())
